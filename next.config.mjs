5c6aabe4748f222061fd899bf98cda38f2914257
manashbarai <manashjbarai@gmail.com>
2025-01-28 23:29:31 +0530

    fix build

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['res.cloudinary.com'], // Add 'res.cloudinary.com' here
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: '',
-          port: '',
-          pathname: '/**',
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    domains: ['res.cloudinary.com'], // Add Cloudinary domain here
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com', // Specify the Cloudinary hostname
+        port: '', // You can leave this empty if it's not needed
+        pathname: '/**', // This will allow all paths under this domain
+      },
+    ],
+  },
+};
+
+export default nextConfig;
